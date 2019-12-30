f0e3b3f91af993879132f7703361be26b808436e
limq <limq@cgnfs.com.cn>
2019-12-30 17:14:54 +0800

    func ok

--- assets.go
+++ assets.go
@@ -8,27 +8,54 @@ import (
 
 var _Assetsbfa8d115ce0617d89507412d5393a462f8e9b003 = "<!doctype html>\r\n<body>\r\n  <p>Can you see this? → {{.Bar}}</p>\r\n  <p>Can you see this 2? → {{.Bar | hAge}}</p>\r\n</body>\r\n"
 var _Assets3737a75b5254ed1f6d588b40a3449721f9ea86c2 = "<!doctype html>\r\n<body>\r\n  <p>Hello, {{.Foo}}</p>\r\n</body>\r\n"
+var _Assets2563e667e12083ecf0c5292d70a6c4de3840819c = "{{define \"goods/good.html\"}}\r\n<html>\r\n<body>\r\n<h1>Goods</h1>\r\n<span>{{.good  }}</span>\r\n</body>\r\n</html>\r\n{{end}}"
+var _Assets76b8f1208fed471ae72580184d170a3791bbeb1f = "{{define \"users/user.html\"}}\r\n<html>\r\n<body>\r\n<h1>Goods</h1>\r\n<span>{{.age | hAge}}</span>\r\n</body>\r\n</html>\r\n{{end}}"
 
 // Assets returns go-assets FileSystem
-var Assets = assets.NewFileSystem(map[string][]string{"/": []string{"html"}, "/html": []string{"bar.tmpl", "index.tmpl"}}, map[string]*assets.File{
-	"/": &assets.File{
+var Assets = assets.NewFileSystem(map[string][]string{"/": []string{"html", "views"}, "/html": []string{"bar.tmpl", "index.tmpl"}, "/views": []string{}, "/views/goods": []string{"good.html"}, "/views/users": []string{"user.html"}}, map[string]*assets.File{
+	"/views/users/user.html": &assets.File{
+		Path:     "/views/users/user.html",
+		FileMode: 0x1b6,
+		Mtime:    time.Unix(1577697215, 1577697215667814100),
+		Data:     []byte(_Assets76b8f1208fed471ae72580184d170a3791bbeb1f),
+	}, "/": &assets.File{
 		Path:     "/",
 		FileMode: 0x800001ff,
-		Mtime:    time.Unix(1577695799, 1577695799995723700),
+		Mtime:    time.Unix(1577697117, 1577697117399451100),
 		Data:     nil,
 	}, "/html": &assets.File{
 		Path:     "/html",
 		FileMode: 0x800001ff,
-		Mtime:    time.Unix(1577695834, 1577695834591983000),
+		Mtime:    time.Unix(1577696993, 1577696993916432800),
+		Data:     nil,
+	}, "/views/goods": &assets.File{
+		Path:     "/views/goods",
+		FileMode: 0x800001ff,
+		Mtime:    time.Unix(1577694270, 1577694270846547100),
+		Data:     nil,
+	}, "/views/goods/good.html": &assets.File{
+		Path:     "/views/goods/good.html",
+		FileMode: 0x1b6,
+		Mtime:    time.Unix(1577694270, 1577694270841667400),
+		Data:     []byte(_Assets2563e667e12083ecf0c5292d70a6c4de3840819c),
+	}, "/views/users": &assets.File{
+		Path:     "/views/users",
+		FileMode: 0x800001ff,
+		Mtime:    time.Unix(1577697215, 1577697215671693300),
 		Data:     nil,
 	}, "/html/bar.tmpl": &assets.File{
 		Path:     "/html/bar.tmpl",
 		FileMode: 0x1b6,
-		Mtime:    time.Unix(1577695834, 1577695834586152200),
+		Mtime:    time.Unix(1577696993, 1577696993916432800),
 		Data:     []byte(_Assetsbfa8d115ce0617d89507412d5393a462f8e9b003),
 	}, "/html/index.tmpl": &assets.File{
 		Path:     "/html/index.tmpl",
 		FileMode: 0x1b6,
 		Mtime:    time.Unix(1574233719, 1574233719973302100),
 		Data:     []byte(_Assets3737a75b5254ed1f6d588b40a3449721f9ea86c2),
+	}, "/views": &assets.File{
+		Path:     "/views",
+		FileMode: 0x800001ff,
+		Mtime:    time.Unix(1577692575, 1577692575079824100),
+		Data:     nil,
 	}}, "")
--- main.go
+++ main.go
@@ -1,44 +1,55 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"html/template"
 	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"strconv"
 	"strings"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	t, err := loadTemplate()
-	if err != nil {
-		panic(err)
-	}
+
 	//r.SetFuncMap(template.FuncMap{
 	//	"hSize": humanSize,
 	//	"hAge":  humanAge,
 	//})
+	//r.LoadHTMLGlob("./views/**/*")
+
+	t, err := loadTemplate()
+	if err != nil {
+		panic(err)
+	}
 	r.SetHTMLTemplate(t)
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "/html/index.tmpl", gin.H{
-			"Foo": "World",
+	r.GET("/users/user", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "users/user.html", gin.H{
+			"age": "1",
 		})
 	})
-	r.GET("/bar", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "/html/bar.tmpl", gin.H{
-			"Bar": "World",
+
+	r.GET("/goods/good", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "goods/good.html", gin.H{
+			"good": "2",
 		})
 	})
-	r.Run(":9091")
+	r.Run("localhost:9091")
+}
+
+func humanSize(s string) string {
+	return strconv.Itoa(rand.Intn(999)) + "mb"
+}
+
+func humanAge(s string) string {
+	return strconv.Itoa(rand.Intn(150)) + "岁"
 }
 
 func loadTemplate() (*template.Template, error) {
 	t := template.New("")
 	for name, file := range Assets.Files {
-		if file.IsDir() || !strings.HasSuffix(name, ".tmpl") {
+		if file.IsDir() || !strings.HasSuffix(name, ".html") {
 			continue
 		}
 		h, err := ioutil.ReadAll(file)
@@ -55,12 +66,4 @@ func loadTemplate() (*template.Template, error) {
 		//}
 	}
 	return t, nil
-}
-
-func humanSize(s string) string {
-	return strconv.Itoa(rand.Intn(999)) + "mb"
-}
-
-func humanAge(s string) string {
-	return strconv.Itoa(rand.Intn(150)) + "岁"
 }
