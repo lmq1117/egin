2610626022cc31f70a31be0c224761ea63659b77
limq <limq@cgnfs.com.cn>
2020-01-07 11:22:52 +0800

    ch8/clock2

--- main.go
+++ main.go
@@ -4,20 +4,40 @@ import (
 	"io"
 	"log"
 	"net"
-	"os"
+	"time"
 )
 
+//$ go build gopl.io/ch8/clock1
+//$ ./clock1 &
+//$ nc localhost 8000
 func main() {
-	conn, err := net.Dial("tcp", "localhost:8001")
+	//net.Listen 对象 监听来自8001端口的连接
+	listener, err := net.Listen("tcp", "localhost:8001")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer conn.Close()
-	mustCopy(os.Stdout, conn)
+
+	for {
+		//listener.Accept方法会直接阻塞，直到新的连接被创建 返回 net.Conn对象来表示这个连接
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+			continue
+		}
+		//处理一个完整的客户端连接
+		go handleConn(conn)
+	}
 }
 
-func mustCopy(dst io.Writer, src io.Reader) {
-	if _, err := io.Copy(dst, src); err != nil {
-		log.Fatal(err)
+func handleConn(c net.Conn) {
+	defer c.Close()
+	//死循环会一直执行，直到写入失败
+	//最可能的原因是客户端主动断开连接
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return
+		}
+		time.Sleep(1 * time.Second)
 	}
 }
