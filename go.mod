71d46a842227bf0d69469819b07817fe1f5b0eb5
limq <limq@cgnfs.com.cn>
2020-01-07 10:16:44 +0800

    goroutines

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module egin
 
 go 1.13
-
-require (
-	github.com/gin-gonic/gin v1.5.0
-	github.com/jessevdk/go-assets v0.0.0-20160921144138-4f4301a06e15
-)
--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("hello")
+	go spinner(100 * time.Millisecond)
+	const n = 1000000
+	fibN := fibC(n)
+	fmt.Printf("\rFibonacci(%d) = %d\n", n, fibN)
+	//fibC(45)
+}
+
+func spinner(delay time.Duration) {
+	for {
+		for _, r := range `\|/` {
+			fmt.Printf("\r%c", r)
+			time.Sleep(delay)
+		}
+	}
+}
+
+func fib(x int) int {
+	if x < 2 {
+		return x
+	} else {
+		return fib(x-1) + fib(x-2)
+	}
+}
+
+func fibC(x int) int {
+	var f []int
+	for i := 0; i < x; i++ {
+		if i < 2 {
+			f = append(f, i+1)
+		} else {
+			f = append(f, f[i-1]+f[i-2])
+		}
+		time.Sleep(time.Millisecond * 100)
+	}
+	//fmt.Println(f)
+	return f[x-2]
 }
